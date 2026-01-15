a43df3d14d208f3918e7a326ab41bd905aef0c41
shahadAlroomi <shahad.ro@icloud.com>
2026-01-15 14:20:18 +0100

    Add registertion method ( Get , Post)

--- Program.cs
+++ Program.cs
@@ -3,15 +3,21 @@ builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
 var app = builder.Build();
 
-app.MapGet("/time", () => DateTime.Now.ToLongTimeString());
+List<Registration> registrations = [];
 
-app.MapGet("/date", () => DateOnly.FromDateTime(DateTime.Now).ToLongDateString());
+app.MapGet("/api/time", () => DateTime.Now.ToLongTimeString());
+
+app.MapGet("/api/date", () => DateOnly.FromDateTime(DateTime.Now).ToLongDateString());
+app.MapGet("/api/registrations", () => registrations);
+app.MapPost("/api/registrations", (Registration rd) => registrations.Add(rd));
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 
 
 app.Run(); // open http://127.0.0.1:5000/index.html
+
+record Registration(string Name , string Email);
 
 //GET / wwwroot/image.png
 //GET /time
