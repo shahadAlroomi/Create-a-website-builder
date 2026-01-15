8a4bf5ff70ab07efc8319b04e37266bd01056cab
shahadAlroomi <shahad.ro@icloud.com>
2026-01-15 11:00:16 +0100

    Add http file (Get -Post)

--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ var app = builder.Build();
 
 app.MapGet("/time", () => DateTime.Now.ToLongTimeString());
 
-
+app.MapGet("/date", () => DateOnly.FromDateTime(DateTime.Now).ToLongDateString());
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 
 
 app.Run(); // open http://127.0.0.1:5000/index.html
+
+//GET / wwwroot/image.png
+//GET /time
